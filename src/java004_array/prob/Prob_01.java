231e5892ff7905aa964b6e1b3519bc968c9e83bc
limhojin880214 <nlimhojin1206@gmail.com>
2022-10-07 18:17:25 +0900

    20221007 | Homework

---
+++ src/java004_array/prob/Prob_01.java
@@ -0,0 +1,25 @@
+package java004_array.prob;
+
+/*
+ * num배열에서 최대값을 출력하는 프로그램을 구현하시오.
+ * [출력결과]
+ * 최대값:95
+ */
+
+public class Prob_01 {
+	public static void main(String[] args) {
+
+		int[] num = { 94, 85, 95, 88, 90 };
+		
+		//여기를 구현하세요.
+		int res = 0;
+		for(int i = 0;i<num.length ; i++) {
+			if(res - num[i] < 0) {
+				res = num[i];
+			}
+		}
+		System.out.printf("최대값:%d", res);
+	
+	}// end main()
+
+}// end class

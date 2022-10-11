8043c21a0af12bf5ab2a01c3eac34335ad2e40e3
limhojin880214 <nlimhojin1206@gmail.com>
2022-10-11 15:38:41 +0900

    20221011 | Update

---
+++ src/java005_method/Java063_method.java
@@ -0,0 +1,35 @@
+package java005_method;
+
+public class Java063_method {
+	public static void main(String[] args) {
+		int[] num = new int[] {5, 2, 8, 7, 4};
+		System.out.printf("최대값:%d\n", max(num));
+		System.out.printf("최소값:%d\n", min(num));
+		System.out.printf("배열의 크기:%d\n", size(num));
+	}//end main()
+	
+	
+	//최대값
+	public static int max(int[] arr) {
+		int res = arr[0];
+		for(int i=0;i <arr.length;i++) {
+			if(res - arr[i] < 0) res =arr[i];
+		}
+		return res;
+	}//end max()
+	
+	//최소값
+	public static int min(int[] arr) {
+		int res = arr[0];
+		for(int i=0;i <arr.length;i++) {
+			if(res - arr[i] > 0) res =arr[i];
+		}
+		return res;
+	}//end min()
+	
+	//배열의 크기
+	public static int size(int[] num) {
+		
+		return num.length;
+	}//end size()
+}//end class

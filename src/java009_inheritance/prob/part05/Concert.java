a84dae120dac72900e5c8037eb6605cfbeefba13
limhojin880214 <nlimhojin1206@gmail.com>
2022-10-15 23:59:14 +0900

    Fin

--- src/java009_inheritance/prob/part05/Concert.java
+++ src/java009_inheritance/prob/part05/Concert.java
@@ -3,24 +3,24 @@ package java009_inheritance.prob.part05;
 public class Concert {
 	String title;
 	String place;
-	String grade;
 	int price;
 	
 	public Concert() {
 	}
-
-	public Concert(String title, String place, String grade, int price) {
+	
+	public Concert(String title, String place, int price) {
 		this.title = title;
 		this.place = place;
-		this.grade = grade;
 		this.price = price;
 	}
 	
-	public String toString() {
-		return String.format("%s %s %s %d", title, place, grade, price);
+	void display() {
+		System.out.printf("%8s\t%8s\t%8d\n", title, place, price);
 	}
 	
-	
+	public String toString() {
+		return String.format("%s %s %d", title, place, price);
+	}
 	
 	
 }
--- src/java009_inheritance/prob/part05/PurchaseHistory.java
+++ src/java009_inheritance/prob/part05/PurchaseHistory.java
@@ -1,19 +1,25 @@
 package java009_inheritance.prob.part05;
 
 public class PurchaseHistory extends Concert{
+	String grade;
 	int quantity;
 	
 	public PurchaseHistory() {
 	}
-	
+	//아래에 구현
 	public PurchaseHistory(String title, String place, String grade, int price, int quantity) {
-		super(title, place, grade, price);
+		super(title, place, price);
+		this.grade = grade;
 		this.quantity = quantity;
+	}
+	
+	public int PurchaseTicket() {
+		return super.price * this.quantity;
 	}
 	
 	@Override
 	public String toString() {
-		return String.format("%s\t%s\t%s\t%d\t%d", super.title, super.place, super.grade, super.price, quantity);
+		return String.format("%s %s %d", super.toString(), grade, quantity);
 	}
 	
 }
